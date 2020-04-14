67906097bc0100d927bbbaa5c1bd85e10f49348e
HayashiKyoka <63498711+HayashiKyoka@users.noreply.github.com>
2020-04-14 13:03:36 +0900

    Update mission_1-1.php

--- mission_1-1.php
+++ mission_1-1.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>mission_1-1</title>
-  </head>
-  <body>
-  <?php
+<?php
     echo "Hello World";
   ?>
-  </body>
-</html>
+
