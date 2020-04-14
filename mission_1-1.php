dbd5a57e758cac065f27badd4e4719f3ca08134a
HayashiKyoka <63498711+HayashiKyoka@users.noreply.github.com>
2020-04-14 11:44:05 +0900

    Create mission_1-1.php

---
+++ mission_1-1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>mission_1-1</title>
+  </head>
+  <body>
+  <?php
+    $hensu="Hello World"
+    echo $hensu;
+  ?>
+  </body>
+</html>
