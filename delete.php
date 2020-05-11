f96c632cffb81dccc518b0f9af500f685c1e136f
HayashiKyoka <63498711+HayashiKyoka@users.noreply.github.com>
2020-05-11 17:07:03 +0900

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <title>detele</title>
+    </head>
+    <body>
+        <form method="POST" action="nextwriting.php">
+    	    名前：<input type="text" name="name"><br>
+    	    コメント：<input type="text" name="comment">
+    	    <input type="submit" value="送信"><br><br>
+        </form>
+        <form method="POST" action="delete.php">
+        	削除対象番号：<input type="text" name="delete">
+    	    <input type="submit" value="削除">
+        </form>
+        <?php
+            $filename = 'board.txt'; //ファイル名
+            if(file_exists($filename)) {//ファイルが存在している
+	            if (!empty($_POST['delete'])){//削除番号が空でない
+                    $delete = $_POST['delete']; //削除対象番号
+                    if($delete === ""){
+                        print "削除対象番号が入力されていません。";
+                    }else{//削除番号に入力されたOK
+                        $lines=file($filename);//ファイルを配列として格納
+			            $fp= fopen($filename, 'w');//ファイルを開く
+				        foreach($lines as $line){//配列の分だけ繰り返し処理　linesは全 体、lineは１行
+				            $new_line=explode("<>",$line);//$line(1行)を<>で分割
+ 					        if($delete==$new_line[0]){//削除番号とファイルの中の投稿番号が同じ
+					        }else{//削除番号とファイルの中の投稿番号が違う
+					        fwrite($fp, $line);
+				    	    }
+                        }fclose($fp);
+		            }
+                }
+                $fp= fopen($filename, 'r'); //ファイルを読み込みモードで開く
+                $lines=file($filename);//ファイルを配列として格納
+	            foreach($lines as $line){//配列の分だけ繰り返し処理　linesは全体、lineは１行
+    	            $new_line=explode("<>",$line);//$line(1行)を<>で分割
+	                echo "$new_line[0]" . ' 名前：' . "$new_line[1]" . ' コメント：' . "$new_line[2]" . ' ' . "$new_line[3]". "<br>" ;//表示
+	            }
+                fclose($fp);
+            }else{//ファイルが存在していない
+                echo "ファイルが存在していません。";
+            }
+        ?>
+    </body>
+</html>
